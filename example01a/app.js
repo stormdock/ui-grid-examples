4c674643868317c569291d2807f80a7f0a1d693f
Michael Angerman <stormasm@gmail.com>
2015-12-05 12:16:21 -0800

    update app with grabbing the data from nedb

--- example01a/app.js
+++ example01a/app.js
@@ -1,7 +1,16 @@
 var app = angular.module('app', ['ngTouch', 'ui.grid']);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
+  var url = 'http://localhost:1212/getangularusers';
+
+  $http.get(url)
+    .success(function(data) {
+      $scope.myData = data;
+    });
+
+// This is another way to get the data statically    
+/*
   $scope.myData = [
     {
         "username": "iris",
@@ -18,5 +27,6 @@ app.controller('MainCtrl', ['$scope', function ($scope) {
         "password": "x3",
         "email": "mike@he.com"
     }
-];
+  ];
+*/
 }]);

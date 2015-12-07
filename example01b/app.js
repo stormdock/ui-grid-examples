959d239a141ebd5b75f195944a98095b9f9f1a00
Michael Angerman <stormasm@gmail.com>
2015-12-07 15:04:56 -0800

    update app

--- example01b/app.js
+++ example01b/app.js
@@ -1,6 +1,28 @@
 var app = angular.module('app', ['ui.grid', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav']);
 
-app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+app.controller('MainCtrl', ['$scope', '$http', '$q', '$interval', function ($scope, $http, $q, $interval) {
+  $scope.gridOptions = {};
+
+  $scope.gridOptions.columnDefs = [
+    { name: 'name', displayName: 'Name (editable)' }
+  ];
+
+  $scope.saveRow = function( rowEntity ) {
+    // create a fake promise - normally you'd use the promise returned by $http or $resource
+    var promise = $q.defer();
+    $scope.gridApi.rowEdit.setSavePromise( rowEntity, promise.promise );
+
+    // fake a delay of 3 seconds whilst the save occurs, return error if gender is "male"
+    $interval( function() {
+      promise.resolve();
+      }, 3000, 1);
+  };
+
+  $scope.gridOptions.onRegisterApi = function(gridApi){
+    //set gridApi on scope
+    $scope.gridApi = gridApi;
+    gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+  };
 
   var url = 'http://localhost:1212/getangularusers';
 
